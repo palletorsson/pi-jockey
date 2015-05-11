c6d650c01da62bfc73457b9de5915f9f09aa7c5f
palle torsson <palle.torsson@gmail.com>
2015-05-11 12:01:46 +0200

    adding time to wait for media server script

--- controller.py
+++ controller.py
@@ -6,6 +6,9 @@ hosts_list =  ["192.168.0.100:5000"] #,"192.168.0.101:5000","192.168.0.102:5000"
 
 # How long is the video?
 video_length = 60 
+startup_time = 20
+
+time.sleep(startup_time) # wait for other ip:s to start
 
 # Loop until we stop the program
 while True: 
