e2eaf84f67a0de1fa627cd1839aebddc4a206ba7
palle torsson <palle.torsson@gmail.com>
2016-02-08 12:00:32 +0100

    adding random principal

--- media-server.py
+++ media-server.py
@@ -2,6 +2,9 @@ from flask import Flask
 import subprocess
 import time
 import settings
+from os import listdir
+from os.path import isfile, join
+from random import randint
 
 print settings.DEFAULT_VIDEO
 
@@ -9,11 +12,15 @@ print "Server started"
 
 app = Flask(__name__)
 
+global transclip
+global videofileplaying
 
-@app.route("/video/start/<int:video_id>/")
-def startVideo(video_id=None):
+mypath = "video"
+onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
+
+@app.route("/video/start/rand/")
+def randVideo(video_id=None):
     print "Incoming video request"
-    global videofileplaying
     try:
         return_code = transclip.poll()
         if return_code == None:
@@ -30,12 +37,44 @@ def startVideo(video_id=None):
     except:
         print "no video playing"
 
-    videofileplaying=subprocess.Popen(["omxplayer", "-b" ,"-o", "hdmi" ,"video/"+settings.DEFAULT_VIDEO], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, close_fds=True)
+    try:
+        randing = randint(0,len(onlyfiles)-1)
+    except:
+        randing = 1
+
+    videofileplaying=subprocess.Popen(["omxplayer", "-b" ,"-o", "hdmi" ,mypath+"/"+onlyfiles[randing], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, close_fds=True)
+    return "Video file played, lol"
+
+
+@app.route("/video/start/<int:video_id>/")
+def startVideo(video_id=None):
+    print "Incoming video request"
+    try:
+        return_code = transclip.poll()
+        if return_code == None:
+            transclip.stdin.write('q')
+            print "Transition was playing, video file stopped"
+    except:
+        print "no transvideo playing"
+
+    try:
+        return_code = videofileplaying.poll()
+        if return_code == None:
+            videofileplaying.stdin.write('q')
+            print "Video was playing, video file stopped"
+    except:
+        print "no video playing"
+
+    try:
+        randing = randint(0,len(onlyfiles)-1)
+    except:
+        randing = 1
+
+    videofileplaying=subprocess.Popen(["omxplayer", "-b" ,"-o", "hdmi" ,mypath+"/"+str(video_id), stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, close_fds=True)
     return "Video file played, lol"
 
 @app.route("/video/stop/")
 def stopVideo():
-    global transclip
 
 
     return_string = "Test : Video stop"
